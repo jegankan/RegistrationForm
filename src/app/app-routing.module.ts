9e37a998bf115538c4231c9c83b4c5c7e503036e
jegankan <jegan.kannann@gmail.com>
2019-02-26 00:43:25 -0600

    Form_Linked_Successfully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ListofdocumentsComponent} from './listofdocuments/listofdocuments.component';
-import {UserregistrationformComponent} from './userregistrationform/userregistrationform.component';
+import { ListofdocumentsComponent } from './listofdocuments/listofdocuments.component';
+import { ListofUsersComponent } from './listofusers/listofusers.component';
+import { UserregistrationformComponent } from './userregistrationform/userregistrationform.component';
+import { RegisterationConfirmationComponent } from './registeration-confirmation/registeration-confirmation.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'Userreg', component: UserregistrationformComponent },
-  { path: 'listofdoc', component:ListofdocumentsComponent },
+  { path: '', redirectTo: '/listofuser', pathMatch: 'full' },
+  { path: 'listofuser', component:ListofUsersComponent },
+  { path: 'listofuser/Userreg', component: UserregistrationformComponent },
+  { path: 'listofuser/Userreg/regconf', component:RegisterationConfirmationComponent },
+  { path: 'listofuser/Userreg/regconf/listofuser/Userreg/listofdoc', component:ListofdocumentsComponent }
 ];
 
 @NgModule({
--- src/app/userregistrationform/userregistrationform.component.ts
+++ src/app/userregistrationform/userregistrationform.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
+ import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-userregistrationform',
   templateUrl: './userregistrationform.component.html',
@@ -9,14 +12,34 @@ import { HttpClient } from '@angular/common/http';
 export class UserregistrationformComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
-
-  ngOnInit() {
+   ngOnInit() {
   }
 
   onSubmit(userregForm){
     console.log(JSON.stringify(userregForm));
-    let userlist={Name:userregForm.Name,Age:userregForm.Age, Email:userregForm.Email};
+    let userlist={"First name":userregForm.inputfirstname,"Last name":userregForm.inputlastname, "SSO ID":userregForm.inputssoid,"Email":userregForm.inputemail};
     this.http.post('http://localhost:3000/employee',userlist).subscribe(status=> console.log(JSON.stringify(status))
     );
   }
-}
+ }
+// @Injectable()
+// export class UserregistrationformComponent implements OnInit {
+
+//   constructor(private injector: Injector,private http: HttpClient) { }
+
+//   handleError(error: any): void {
+//     const routerService = this.injector.get(Router);
+//     const ngZone = this.injector.get(NgZone);
+//     ngZone.run(() => {
+//       routerService.navigate(['/Userreg'], { skipLocationChange: true });
+//     });
+//   }
+//   ngOnInit() {
+//        }
+//   onSubmit(userregForm){
+//     console.log(JSON.stringify(userregForm));
+//     let userlist={"First name":userregForm.inputfirstname,"Last name":userregForm.inputlastname, "SSO ID":userregForm.inputssoid,"Email":userregForm.inputemail};
+//     this.http.post('http://localhost:3000/employee',userlist).subscribe(status=> console.log(JSON.stringify(status))
+//     );
+//   }
+// }
